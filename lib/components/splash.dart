63b0642037fdc9640e379cf7972921d8cf346125
Harsh Dusane <harshdusane5@gmail.com>
2024-06-09 23:09:13 +0530

    Initial commit

---
+++ lib/components/splash.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              height: 811,
+              width:386,
+              decoration: BoxDecoration(
+              color: Colors.green,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                    image:
+                AssetImage('assets/images/had.jpg'))
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
